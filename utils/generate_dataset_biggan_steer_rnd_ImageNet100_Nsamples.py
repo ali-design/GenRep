327ba5f6ae8f200794f835adc4804160dbeadc78
Ali Jahanian <ali-design@csail.mit.edu>
2020-10-26 16:40:24 -0400

    generate truncated gaussinas with N samples per anchor

--- utils/generate_dataset_biggan_steer_rnd_ImageNet100_Nsamples.py
+++ utils/generate_dataset_biggan_steer_rnd_ImageNet100_Nsamples.py
@@ -31,7 +31,7 @@ def convert_to_images(obj):
         img.append(PIL.Image.fromarray(out_array))
     return img
 
-def truncated_noise_sample_neighbors(batch_size=1, dim_z=128, truncation=1., seed=None):
+def truncated_noise_sample_neighbors(batch_size=1, dim_z=128, truncation=1., seed=None, num_neighbors=20):
     """ Create a truncated noise vector.
         Params:
             batch_size: batch size.
@@ -41,20 +41,22 @@ def truncated_noise_sample_neighbors(batch_size=1, dim_z=128, truncation=1., see
         Output:
             array of shape (batch_size, dim_z)
     """
+    list_results = []
     state = None if seed is None else np.random.RandomState(seed)
     values = truncnorm.rvs(-2, 2, size=(batch_size, dim_z), random_state=state).astype(np.float32)
-    norm_1 = np.random.normal(0., 1.1, size=(batch_size, dim_z)).astype(np.float32)
-    norm_2 = np.random.normal(0., 1.2, size=(batch_size, dim_z)).astype(np.float32)
-    norm_3 = np.random.normal(0., 1.3, size=(batch_size, dim_z)).astype(np.float32)
-    norm_4 = np.random.normal(0., 1.4, size=(batch_size, dim_z)).astype(np.float32)
-
     zs = truncation * values
 
-    return [zs, zs + norm_1, zs + norm_2, zs + norm_3, zs + norm_4]
+    state_neighbors = None if seed is None else np.random.RandomState(seed+1000)
+    for i in range(num_neighbors):
+        state_neighbors = None if seed is None else np.random.RandomState(seed+1000+i)
+        values_neighbors = truncation * truncnorm.rvs(-2, 2, size=(batch_size, dim_z), random_state=state_neighbors).astype(np.float32)
+        list_results.append(zs + values_neighbors)
+
+    return list_results
 
 def sample(opt):
-    output_path = (os.path.join(opt.out_dir, 'biggan%dtr%d-%s_%s' %
-                   (opt.size, int(opt.truncation), opt.imformat, opt.desc)))
+    output_path = (os.path.join(opt.out_dir, 'biggan%dtr%d-%s_%s_%d_samples' %
+                   (opt.size, int(opt.truncation), opt.imformat, opt.desc, opt.num_neighbors)))
     partition = opt.partition
     # start_seed, nimg = constants.get_seed_nimg(partition)
     start_seed = opt.start_seed
@@ -82,7 +84,7 @@ def sample(opt):
         seed = start_seed + idx
         noise_vector_neighbors = truncated_noise_sample_neighbors(truncation=truncation,
                                                         batch_size=nimg,
-                                                        seed=seed)
+                                                        seed=seed, num_neighbors=opt.num_neighbors)
         class_vector = torch.from_numpy(class_vector).cuda()
         for ii in range(len(noise_vector_neighbors)):
             noise_vector = noise_vector_neighbors[ii]
@@ -95,28 +97,20 @@ def sample(opt):
                 output = output.cpu()
                 ims = convert_to_images(output)
                 for i, im in enumerate(ims):
-                    if ii == 0:
-                        im_name = 'seed%04d_sample%05d_anchor.%s' % (seed, batch_start+i, imformat)
-                    elif ii == 1:
-                        im_name = 'seed%04d_sample%05d_1.1.%s' % (seed, batch_start+i, imformat)
-                    elif ii == 2:
-                        im_name = 'seed%04d_sample%05d_1.2.%s' % (seed, batch_start+i, imformat)
-                    elif ii == 3:
-                        im_name = 'seed%04d_sample%05d_1.3.%s' % (seed, batch_start+i, imformat)
-                    elif ii == 4:
-                        im_name = 'seed%04d_sample%05d_1.4.%s' % (seed, batch_start+i, imformat)
+                    im_name = 'seed%04d_sample%05d_1.0_%d.%s' % (seed, batch_start+i, ii, imformat)
                     im.save(os.path.join(class_dir_name, im_name))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Sample from biggan")
     parser.add_argument('--out_dir', default='/data/scratch/jahanian/ganclr_results_2/', type=str)
-    parser.add_argument('--partition', default='val', type=str)
+    parser.add_argument('--partition', default='train', type=str)
     parser.add_argument('--truncation', default=1.0, type=float)
     parser.add_argument('--size', default=256, type=int)
     parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--imformat', default='png', type=str)
-    parser.add_argument('--num_imgs', default=50, type=int, help='num imgs per class')
-    parser.add_argument('--start_seed', default=500, type=int)
+    parser.add_argument('--num_imgs', default=1300, type=int, help='num imgs per class')
+    parser.add_argument('--start_seed', default=0, type=int)
+    parser.add_argument('--num_neighbors', default=20, type=int, help='num samples per anchor')
     parser.add_argument('--desc', default='steer_rnd_100', type=str, help='this will be the tag of this specfic dataset, added to the end of the dataset name')
     opt = parser.parse_args()
     sample(opt)
